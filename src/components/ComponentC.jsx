8d567f50223fdc727a697c1a8bf1446e064db115
Farhan Romli <farhan_romli@outlook.com>
2023-08-05 22:10:58 +0800

    Create ComponentC file

---
+++ src/components/ComponentC.jsx
@@ -0,0 +1,9 @@
+const ComponentC = ({objekSaya}) => {
+    return <div>
+        <p>Nama saya: {objekSaya.nama}</p>
+        <p>Umur saya: {objekSaya.umur}</p>
+        
+    </div>
+}
+
+export default ComponentC
